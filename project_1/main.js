a52542e0b48f12cef50054323bf6a93a5fd49755
Grzegorz <grzeslach8@gmail.com>
2024-02-06 23:38:20 +0100

    JS Completed

--- project_1/main.js
+++ project_1/main.js
@@ -1,14 +1,32 @@
-document.querySelectorAll('li').forEach(item => {
+document.querySelectorAll(' ul > li ').forEach(item => {
+    const indicator = document.getElementById("indicator");
     item.addEventListener('mouseover', function() {
       const childUl = this.querySelector('ul');
+      
       if (childUl) {
+        document.querySelectorAll(' ul > li > ul ').forEach(item => {
+            if(item.classList.contains("submenu")){
+                item.style.display = "none";
+            }
+        });
         childUl.style.display = 'flex'; 
+         indicator.style.display = "block";
+         indicator.style.top = item.getBoundingClientRect().top + 50 + "px"; 
+         indicator.style.left = item.getBoundingClientRect().left + 30 + "px"; 
       }
+    
     });
   });
 document.querySelectorAll('li > ul').forEach(item => {
     item.addEventListener('mouseout', function() {
-        item.style.display = 'none'; 
+        if(item.classList.contains("submenu"))
+        {
+            item.style.display = 'none'; 
+            indicator.style.display = "none";
+        }
+       
+
     });
   });
   
+
