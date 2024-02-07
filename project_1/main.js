8afbf112999c757cabdc028cea57b51e192ab7e5
Grzegorz <grzeslach8@gmail.com>
2024-02-07 19:14:46 +0100

    Tweak. Submenu closes when tries to open another one

--- project_1/main.js
+++ project_1/main.js
@@ -1,32 +1,33 @@
-document.querySelectorAll(' ul > li ').forEach(item => {
-    const indicator = document.getElementById("indicator");
-    item.addEventListener('mouseover', function() {
-      const childUl = this.querySelector('ul');
-  
-      if (childUl) {
-        document.querySelectorAll(' ul > li > ul ').forEach(item => {
-            
-        });
-        childUl.style.display = 'flex'; 
-         indicator.style.display = "block";
-         item.dataset.active = "true";
-         indicator.style.top = item.getBoundingClientRect().top + 50 + "px"; 
-         indicator.style.left = item.getBoundingClientRect().left + 30 + "px"; 
-      }
-    
+document.querySelectorAll('ul > li').forEach(item => {
+  const indicator = document.getElementById("indicator");
+  const hideAllSubmenus = () => { 
+    document.querySelectorAll('.submenu').forEach(menu => {
+      menu.style.display = "none";
+      indicator.style.display = "none";
     });
-  });
-document.querySelectorAll('li > ul').forEach(item => {
-    item.addEventListener('mouseout', function() {
-        if(item.classList.contains("submenu"))
-        {
-            item.style.display = 'none'; 
-            indicator.style.display = "none";
-             item.dataset.active = "false";
-        }
-       
+  };
 
-    });
-  });
-  
+  item.addEventListener('mouseover', function() {
+    const childUl = this.querySelector('ul');
 
+    hideAllSubmenus();
+
+    if (childUl) {
+      childUl.style.display = 'flex';
+      indicator.style.display = "block";
+      item.dataset.active = "true";
+      indicator.style.top = item.getBoundingClientRect().top + 50 + "px";
+      indicator.style.left = item.getBoundingClientRect().left + 30 + "px";
+    }
+  });
+});
+
+document.querySelectorAll('li > ul').forEach(submenu => {
+  submenu.addEventListener('mouseout', function(e) {
+    if (e.relatedTarget.tagName.toLowerCase() !== 'ul') {
+      this.style.display = 'none';
+      indicator.style.display = "none";
+      this.dataset.active = "false";
+    }
+  });
+});
